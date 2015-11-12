0b644ac06db41fd7557e02de9691cae7d1789f5d
Peter Wagner <pwagner@pebble.com>
2015-11-11 20:18:35 -0500

    JSON, 3.0.0

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 
 let server = http.createServer(function(req, res) {
   let message = process.env.MESSAGE || 'unknown';
-  res.end(`Version 2.0.0, message "${message}"`);
+  res.end(JSON.stringify({version: '3.0.0', message: message }));
 });
 
 
